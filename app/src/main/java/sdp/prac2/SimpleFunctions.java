31d0908c47b18934253ea6ecb163531d7122e88c
Tebogo Nomnqa <tebzax2@gmail.com>
2023-04-15 14:23:05 +0200

    add task3 function

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,19 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    // returns true only if there are a matching number of ( and ) brackets in a string, and those brackets are nested correctly.
+    public boolean task3(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else if (s.charAt(i) == ')') {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return count == 0;
+    }
 }
