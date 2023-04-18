6aa54b38aa8bb05721b166bb39dd93926bef7c81
Uviwe Jumba <G20J1104@ict.ru.ac.za>
2023-04-18 12:39:10 +0200

    Added method for task 5

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,21 +4,7 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
-    // returns true only if there are a matching number of ( and ) brackets in a string, and those brackets are nested correctly.
-    public boolean task3(String s) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                count++;
-            } else if (s.charAt(i) == ')') {
-                count--;
-            }
-            if (count < 0) {
-                return false;
-            }
-        }
-        return count == 0;
-    }
+   
 
     public int task1(List<Integer> list1, List<Integer> list2) {
         int sum = 0;
@@ -38,5 +24,36 @@ public class SimpleFunctions {
         }
         return result;
     } //Task 2
+
+
+
+     // returns true only if there are a matching number of ( and ) brackets in a string, and those brackets are nested correctly.
+     public boolean task3(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else if (s.charAt(i) == ')') {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return count == 0;
+    }
+
+
+
+    public static boolean task5(List<Integer> list) {
+        // Returns true if the list is sorted
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
